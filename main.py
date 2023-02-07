7eacbe01429cbdaa2945c9b592dbfb953e00d5e4
Luiz Esquivel <62815987+luizesquivel05@users.noreply.github.com>
2023-02-06 21:44:00 -0300

    leitura de vidas e quantidade palavras
    Aqui iremos criar a quantidade de vidas e quantidade de palavras

---
+++ main.py
@@ -0,0 +1,9 @@
+# LEITURA DA QUANIDADE DE VIDAS:
+vidas = int(input("Informe O numero de vidas: ")) 
+while vidas <= 0:
+    vidas = int(input("O número de vidas deve ser maior ou igual a 1: "))
+
+# LEITURA DA QUANTIDADE DE PALAVRAS:
+qtd = int(input("Digite a quantidade de palavras: "))
+while qtd <= 0:
+    qtd = int(input("A quantidade de palavras deve ser maior ou igual a 1: "))
